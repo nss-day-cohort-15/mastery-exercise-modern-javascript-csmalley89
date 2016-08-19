d30b6ddd66a5e5e1e75901e2d7c5594caf8e09e5
Chris Smalley <chrism.smalley@gmail.com>
2016-08-19 16:01:42 -0500

    initial html setup

---
+++ js/app.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+
+
+// Navigation
+
+  $("#homeScreen").show();
+// When any button with nextButton class is clicked, move on to the next view.
+
+  $(".nextButton").click(function(e) {
+    var nextCard = $(this).attr("next");
+    var moveAlong = true;
+    switch (nextCard) {
+      case "robotPage":
+        moveAlong = ($("#userName").val() !== "");
+        break;
+      case "weaponPage":
+        moveAlong = ($("#userName").val() !== "");
+        break;
+      case "battlePage":
+        moveAlong = ($("#userName").val() !== "");
+        battleCards()
+        break;
+    }
+
+    if (moveAlong) {
+      $(".card").hide();
+      $("." + nextCard).show();
+    }
+  });
+// Back Button Event listner
+  $(".backButton").click(function(e) {
+    var previousCard = $(this).attr("back");
+    $(".card").hide();
+    $("." + previousCard).show();
+  });
+
+})
+
