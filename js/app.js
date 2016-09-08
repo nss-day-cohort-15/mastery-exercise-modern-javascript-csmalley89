4c4c6ed4ea206a05e54340e94fd53ed3074a7b48
Chris Smalley <chrism.smalley@gmail.com>
2016-09-07 20:34:35 -0500

    app works, still has some bugs

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 
 var Robot = (function(robot){
+  $(document).ready(function() {
+    var players = [];
+    var p1;
+    var p2;
+    var p1Name;
 
- $(document).ready(function(){
-// Navigation
 
-  $("#homeScreen").show();
-  // When any button with nextButton class is clicked, move on to the next view.
+    // Navigation
+
+    $("#homeScreen").show();
+    // When any button with nextButton class is clicked, move on to the next view.
 
     $(".nextButton").click(function(e) {
       var nextCard = $(this).attr("next");
@@ -14,9 +19,6 @@ var Robot = (function(robot){
         case "robotPage":
           moveAlong = ($("#userName").val() !== "");
           break;
-        // case "weaponPage":
-        //   moveAlong = ($("#userName").val() !== "");
-        //   break;
         case "battlePage":
           moveAlong = ($("#userName").val() !== "");
           battleCards()
@@ -28,38 +30,73 @@ var Robot = (function(robot){
         $("." + nextCard).show();
       }
     });
-  // Back Button Event listner
+    // Back Button Event listner
     $(".backButton").click(function(e) {
       var previousCard = $(this).attr("back");
       $(".card").hide();
       $("." + previousCard).show();
     });
 
-  // Captures userName
-      $("[next='robotPage']").on('click', function(e) {
+    // Captures userName
+    $("[next='robotPage']").on('click', function(e) {
 
-        var p1Name = $('#userName').val();
-        console.log(p1Name)
+      p1Name = $('#userName').val();
+      console.log(p1Name)
     });
+    // Allows users to select Player 1 and Player 2 robots
+      $('.robotCard').on('click', function() {
+        if (players.length == 0) {
+          $(this).addClass('p1');
+          $(".cardContent", this).html("Player 1");
+          var selectedBot = $(this).attr("id");
+          p1 = new Robot[selectedBot]();
+          players.push(p1);
+          console.log("player 1", p1);
+        } else if (players.length == 1) {
+            $(this).addClass('p2');
+            $(".cardContent", this).html("Player 2");
+            var selectedBot = $(this).attr("id");
+            p2 = new Robot[selectedBot]();
+            players.push(p2);
+            console.log("player 2", p2);
+        } else {
+          $('.robotCard').off('click');
+        };
+      });
+  // BATTLEGROUND FUNCTIONS
+    function battleCards() {
+      printStats(p1, p2);
+    };
+
+    function printStats(p1, p2){
+      var playerOutput = `<div class="battleCard"><h3>${p1.name}<br><small> Fighting for ${p1Name}</small></h3><br><p>${p1.name} has an attack strenth of ${p1.attack}</p><br>` +
+                     `Health: ${p1.health}<br>${p1.img}</div>`;
+      $(".p1Stats").html(playerOutput);
+
+      var enemyOutput = `<div class="battleCard"><h3>${p2.name}<br><small> Fighting against mankind!</small></h3><br><p>${p2.name} has an attack strenth of ${p2.attack}</p><br>` +
+                     `Health: ${p2.health}<br>${p2.img}</div>`;
+      $(".p2Stats").html(enemyOutput);
+    }
+
+    $("#attack").on("click", function (){
+      console.log("p2 health", p2.health)
+      console.log("p2 attack", p2.attack)
+
+      console.log("p1 health", p1.health)
+      console.log("p1 attack", p1.attack)
+      p2.health = p2.health - p1.attack
+      p1.health = p1.health - p2.attack
+      printStats(p2, p1)
+
+      if ((p2.health) <= 0) {
+        alert("Game over. You win!")
+      } else if (p1.health <= 0) {
+        alert("Game over. You lose.")
+      }
+    })
 
 
-  // Allows users to select Player 1 and Player 2 robots
-    $('.robotCard').on('click', function(a){
-      $(this).addClass('p1');
-      $(".cardContent", this).html("Player 1");
-      console.log("Player 1 is", this.id);
-      $('.robotCard').on("click", function(b){
-        $(this).removeClass('p1')
-        $(this).addClass('p2');
-        $(".cardContent", this).html("Player 2");
-        console.log("Player 2 is", this.id);
-      });
-      $('.robotCard').click(function(){
-        $('.robotCard').off('click');
-      });
-    });
 
   });
-
 })(Robot || {});
 
--- js/robots.js
+++ js/robots.js
@@ -1,6 +1,7 @@
 
 var Robot = (function(robot){
 
+
   // Base Robot Function
   robot.BaseBot = function() {
     this.name = null;
@@ -14,101 +15,112 @@ var Robot = (function(robot){
   // high heath, low attack
   robot.GoodGuy = function(){
     this.class = "Good Guy League"
-    this.health = Math.floor(Math.random() * (120 - 60) + 60);
-    this.attack = Math.floor(Math.random() * (80 - 50) + 50);
+    this.health = Math.floor(Math.random() * (120 - 60) + 60)*2;
+    this.attack = Math.floor(Math.random() * (80 - 50) + 50)/2;
   };
   robot.GoodGuy.prototype = new robot.BaseBot();
 
   robot.Bender = function(){
     this.name = "Bender";
-    this.img = `<img>/../img/bender.png />`;
+    this.img = `<img src="/../img/bender.png">`;
     this.health += -5;
-    this.damage += 5;
+    this.attack += 5;
   };
-  robot.Bender = new robot.GoodGuy();
+  robot.Bender.prototype = new robot.GoodGuy();
+
 
   robot.Url = function(){
     this.name = "URL";
-    this.img = `<img>/../img/url.png />`;
+    this.img = `<img src="/../img/url.png">`;
     this.health += 0;
-    this.damage += 10;
+    this.attack += 10;
   };
-  robot.Url = new robot.GoodGuy();
+  robot.Url.prototype = new robot.GoodGuy();
+
 
   robot.KwanzaaBot = function(){
     this.name = "Kwanzaa Bot";
-    this.img = `<img>/../img/kwanzaaBot.png />`;
+    this.img = `<img src="/../img/kwanzaaBot.png">`;
     this.health += 5;
-    this.damage += -10;
+    this.attack += -10;
   };
-  robot.KwanzaaBot = new robot.GoodGuy();
+  robot.KwanzaaBot.prototype = new robot.GoodGuy();
+
 
   // EVIL BOTS
   // low health, high attack
   robot.EvilBot = function(){
     this.class = "Evil League"
-    this.health = Math.floor(Math.random() * (80 - 50) + 50);
-    this.attack = Math.floor(Math.random() * (120 - 60) + 60);
+    this.health = Math.floor(Math.random() * (80 - 50) + 50)*2;
+    this.attack = Math.floor(Math.random() * (120 - 60) + 60)/2;
   };
   robot.GoodGuy.prototype = new robot.BaseBot();
 
   robot.Devil = function(){
     this.name = "Robot Devil";
-    this.img = `<img>/../img/robotDevil.png />`;
+    this.img = `<img src="/../img/robotDevil.png">`;
     this.health += -5;
-    this.damage += 5;
+    this.attack += 5;
   };
-  robot.Devil = new robot.EvilBot();
+  robot.Devil.prototype = new robot.EvilBot();
+
 
   robot.Santa = function(){
     this.name = "Robot Santa";
-    this.img = `<img>/../img/santa.png />`;
+    this.img = `<img src="/../img/santa.png">`;
     this.health += 0;
-    this.damage += 10;
+    this.attack += 10;
   };
-  robot.Santa = new robot.EvilBot();
+  robot.Santa.prototype = new robot.EvilBot();
 
   robot.Roberto = function(){
     this.name = "Roberto";
-    this.img = `<img>/../img/roberto.png />`;
+    this.img = `<img src="/../img/roberto.png">`;
     this.health += 5;
-    this.damage += -10;
+    this.attack += -10;
   };
-  robot.Roberto = new robot.EvilBot();
+  robot.Roberto.prototype = new robot.EvilBot();
+
 
   // BATTLE LEAGUE
   // med health, med attack
   robot.BattleBot = function(){
     this.class = "Battle League"
-    this.health = Math.floor(Math.random() * (100 - 55) + 55);
-    this.attack = Math.floor(Math.random() * (100 - 55) + 55);
+    this.health = Math.floor(Math.random() * (100 - 55) + 55)*2;
+    this.attack = Math.floor(Math.random() * (100 - 55) + 55)/2;
   };
   robot.GoodGuy.prototype = new robot.BaseBot();
 
+
   robot.BillionaireBot = function(){
     this.name = "BillionaireBot";
-    this.img = `<img>/../img/billionaireBot.png />`;
+    this.img = `<img src="/../img/billionaireBot.png">`;
     this.health += -5;
-    this.damage += 5;
+    this.attack += 5;
   };
-  robot.BillionaireBot = new robot.BattleBot();
+  robot.BillionaireBot.prototype = new robot.BattleBot();
+
 
   robot.Destructor = function(){
     this.name = "Destructor";
-    this.img = `<img>/../img/destructor.png />`;
+    this.img = `<img src="/../img/destructor.png">`;
     this.health += 0;
-    this.damage += 10;
+    this.attack += 10;
   };
-  robot.Destructor = new robot.BattleBot();
+  robot.Destructor.prototype = new robot.BattleBot();
+
+
 
   robot.Clearcutter = function(){
     this.name = "Clearcutter";
-    this.img = `<img>/../img/clearcutter.png />`;
+    this.img = `<img src="/../img/clearcutter.png">`;
     this.health += 5;
-    this.damage += -10;
+    this.attack += -10;
   };
-  robot.Clearcutter = new robot.BattleBot();
+  robot.Clearcutter.prototype = new robot.BattleBot();
+
 
   return robot;
+
 
 })(Robot || {});
